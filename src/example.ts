54345ac35fd2c77f7643107ae6f5745d28494db9
GoodbyeAntwerpblue <daishi3741@gmail.com>
2024-01-22 16:47:39 +0900

    fix css

--- src/example.ts
+++ src/example.ts
@@ -1,16 +1,22 @@
-'use strict';
-import {ipcMain, app, BrowserWindow, IpcMainEvent} from "electron";
-const {startPrint} = require("./print");
+"use strict";
+import { ipcMain, app, BrowserWindow, IpcMainEvent } from "electron";
+const { startPrint } = require("./print");
 import path from "path";
 import fs from "fs";
-import {pathToFileURL} from "url";
+import { pathToFileURL } from "url";
 
-const examplePage = path.resolve(app.getPath("userData"), 'electron-print-preview', 'example.html')
+const examplePage = path.resolve(
+  app.getPath("userData"),
+  "electron-print-preview",
+  "example.html"
+);
 const exampleDirname = path.dirname(examplePage);
 if (!fs.existsSync(exampleDirname)) {
-    fs.mkdirSync(exampleDirname, {recursive: true})
+  fs.mkdirSync(exampleDirname, { recursive: true });
 }
-fs.writeFileSync(examplePage, `
+fs.writeFileSync(
+  examplePage,
+  `
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -42,49 +48,48 @@ fs.writeFileSync(examplePage, `
     })
 </script>
 </html>
-`)
+`
+);
 
 var example = {};
 
-
 function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    transparent: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
 
-    // Create the browser window.
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        transparent: true,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
+  // and load the index.html of the app.
+  mainWindow.loadURL(pathToFileURL(examplePage).href);
 
-    // and load the index.html of the app.
-    mainWindow.loadURL(pathToFileURL(examplePage).href);
-
-    ipcMain.handle('startPrint', async (e: IpcMainEvent, html: string) => {
-        startPrint({htmlString: html}, e)
-    })
+  ipcMain.handle("startPrint", async (e: IpcMainEvent, html: string) => {
+    startPrint({ htmlString: html }, e);
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-    createWindow();
-    app.on('activate', function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+  createWindow();
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
 });
 
 module.exports = example;
